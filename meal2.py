7ff6158d431b3ae79eb0fce258d3046a51c68021
brainjun <ywysgsu28@gmail.com>
2024-05-20 22:40:52 +0900

    new

---
+++ meal2.py
@@ -0,0 +1,34 @@
+#pip install BeautifulSoup4 lxml
+
+import requests
+from bs4 import BeautifulSoup
+import os
+from PIL import Image
+import time
+
+url = "https://ibook.kpu.ac.kr/Viewer/menu02"
+headers = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"}
+req = requests.get(url, headers=headers)
+soup = BeautifulSoup(req.text, 'lxml')
+bookcode = soup.find(attrs={"name":"bookcode"}).get("value")
+
+req = requests.get(f"https://ibook.kpu.ac.kr/Viewer/getBookXML/{bookcode}", headers=headers)
+srcs = [req.json()[0]['src'], req.json()[1]['src']]
+path = os.path.dirname(os.path.abspath(__file__))
+
+for i in range(2):
+    file_path = f'{i}.jpg'
+
+    if os.path.exists(file_path):
+        os.remove(file_path)
+
+time.sleep(1)
+
+for i in range(0, len(srcs)):
+    imgurl = "https:" + srcs[i]
+    req = requests.get(imgurl, headers=headers)
+    with open(os.path.join(path, f"{i}.jpg"), "wb") as f:
+        f.write(req.content)
+for i in range(2):
+    img = Image.open(f'{i}.jpg')
+    img.show()       
