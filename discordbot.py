2f0ab21d5e5009e65e4c5220309b871d9b493c44
2coconut <wosjiwjs@gmail.com>
2024-05-23 20:13:50 +0900

    60% 완성
    열차 시간표만 구현하면 끝

--- discordbot.py
+++ discordbot.py
@@ -13,18 +13,44 @@ async def on_ready():
     print("The bot is ready!")
 
 @bot.slash_command()
-async def 열차시간(inter):
+async def 수인분당열차시간(inter):
     try:
-        station()
+        await inter.response.send_message("미완성")
+
+
+
     except Exception as e:
         await inter.response.send_message(f"오류가 발생했습니다...!: {e}")
 
 @bot.slash_command()
-async def 학식(inter):
+async def 수도권4호선열차시간(inter):
     try:
-        bs4_meal()
+        await inter.response.send_message("미완성")
+
+
+
     except Exception as e:
         await inter.response.send_message(f"오류가 발생했습니다...!: {e}")
+
+@bot.slash_command()
+async def tip학식(inter):
+    try:
+        bs4_meal.get_meal()
+        file_path_tip = './0.jpg'
+        img_file_tip = disnake.File(file_path_tip)
+        await inter.response.send_message(file=img_file_tip)
+    except Exception as e:
+        await inter.response.send_message(f"오류가 발생했습니다...!: \n {e}")
+
+@bot.slash_command()
+async def e동학식(inter):
+    try:
+        bs4_meal.get_meal()
+        file_path_E = "./1.jpg"
+        img_file_E = disnake.File(file_path_E)
+        await inter.response.send_message(file=img_file_E)
+    except Exception as e:
+        await inter.response.send_message(f"오류가 발생했습니다...!: \n {e}")
 
 
 bot.run(BOT_TOKEN)
